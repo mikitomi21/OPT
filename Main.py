c063267ea66bba0e8e7492ae3f9d69d5d63bebe7
mikitomi21 <kubastach2001@gmail.com>
2022-06-10 14:30:06 +0200

    Hermatyzjacja
    private _ lub bardzije __

--- Main.py
+++ Main.py
@@ -1,1 +1,18 @@
-print("Hello OPTs!!!")
+class Test:
+    __list = []
+    def addElement(self, arg):
+        self.__list.append(arg)
+
+    def popBack(self):
+        if len(self.__list):
+            return self.__list.pop(len(self.__list)-1)
+
+obj = Test()
+
+obj.addElement(23)
+obj.addElement('a')
+obj.addElement("ala")
+obj._Test__list.append("elo")
+print(obj._Test__list)
+obj.popBack()
+print(obj._Test__list)

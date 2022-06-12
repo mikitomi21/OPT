6a169fb475df3a15f02806e76c3dc659447b3d67
mikitomi21 <kubastach2001@gmail.com>
2022-06-12 15:23:59 +0200

    Add getters and setters

--- Main.py
+++ Main.py
@@ -4,5 +4,10 @@ from Point import *
 name = input("Name of your character: ")
 player = Player(name)
 
-while player.isAlive:
-    print("aaa")
+while player.get_isAlive():
+    print(f"Health: {player.get_health()}")
+    print(f"Power: {player.get_power()}")
+    print(f"Loyal: {player.get_loyal()}")
+    print(f"Money: {player.get_money()}")
+    
+    player.set_isAlive()
--- Player.py
+++ Player.py
@@ -1,10 +1,59 @@
 class Player:
     def __init__(self, name):
         self.name = name
-        #Skala wszystkich umiejetnosci 0-100
-        self.power = 10
-        self.agility = 10
-        self.luck = 10
-        self.stamina = 100
-        self.money = 0
-        self.isAlive = True
+        self._age = 0
+        # Skala wszystkich umiejetnosci 0-20
+        self._health = 10
+        self._power = 10
+        self._loyal = 10
+        self._money = 10
+        self._isAlive = True
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, age):
+        self._age = age
+
+    def get_health(self):
+        return self._health
+
+    def add_health(self, health):
+        if self._health + health >= 20:
+            self._health = 20
+        else:
+            self._health += health
+
+    def get_power(self):
+        return self._power
+
+    def add_power(self, power):
+        if self._power + power >= 20:
+            self._power = 20
+        else:
+            self._power += power
+
+    def get_loyal(self):
+        return self._loyal
+
+    def add_loyal(self, loyal):
+        if self._loyal + loyal >= 20:
+            self._loyal = 20
+        else:
+            self._loyal += loyal
+
+    def get_money(self):
+        return self._money
+
+    def add_money(self, money):
+        if self._money + money >= 20:
+            self._money = 20
+        else:
+            self._money += money
+
+    def get_isAlive(self):
+        return self._isAlive
+
+    def set_isAlive(self):
+        if self._health <= 0 or self._power <= 0 or self._loyal <= 0 or self._money <= 0:
+            self._isAlive = False
